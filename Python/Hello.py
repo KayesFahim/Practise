06c842dc8feaaf73e031de6222fa6a491699cabf
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-02-26 16:20:14 -0500

    Pythone running

---
+++ Python/Hello.py
@@ -0,0 +1,15 @@
+print("Python working successfully")
+
+list= ["kayes", "fahim", "DJkaka"]
+print(list)
+
+dict ={
+    'name' : "Kayes",
+    'age' : "39",
+    'cosso' : "BJ",
+    'top' : "cse"}
+
+print(dict)
+
+for x, y in dict.items():
+  print(x, y)
