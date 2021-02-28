3710a7c47c981fc00613b2020ceaa3c00c33b2f7
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-02-28 15:42:07 -0500

    Automation

---
+++ Python/Automation by Sillenium/automation.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+chrome_driver = webdriver.Chrome('chromedriver')
+print(chrome_driver)
+
+chrome_driver.maximize_window()
+chrome_driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+
+assert 'Show Message' in chrome_driver.page_source
+
+user_message = chrome_driver.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I am kayes Fahim')
+
